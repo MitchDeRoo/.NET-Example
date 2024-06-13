76992c150da65f27277d79709c1e1ee04d5aaa2b
MitchDeRoo <deroom01@gmail.com>
2024-06-13 13:46:01 +1000

    chore: Updates Test Project to use XUnit

--- Foundation/Foundation.Tests/UnitTest1.cs
+++ Foundation/Foundation.Tests/UnitTest1.cs
@@ -1,15 +1,9 @@
 namespace Foundation.Tests;
 
-public class Tests
+public class UnitTest1
 {
-    [SetUp]
-    public void Setup()
-    {
-    }
-
-    [Test]
+    [Fact]
     public void Test1()
     {
-        Assert.Pass();
     }
 }

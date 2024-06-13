2dc4ca99ea0bbec95497804903f971b360954f19
MitchDeRoo <deroom01@gmail.com>
2024-06-14 08:18:34 +1000

    chore: Adds Data Annotations for Anomaly, Researcher Models

--- Foundation/Foundation/Anomalies/Anomaly.cs
+++ Foundation/Foundation/Anomalies/Anomaly.cs
@@ -1,9 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Foundation.Anomalies;
 
 public class Anomaly
 {
     public Guid Id { get; set; }
+
+    [Required]
+    [StringLength(30, MinimumLength = 5)]
     public string Name { get; set; }
+
+    [Required]
     public string ObjectClass { get; set; }
     public string Description { get; set; }
 }
--- Foundation/Foundation/Researchers/Researcher.cs
+++ Foundation/Foundation/Researchers/Researcher.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using Foundation.Anomalies;
 
 namespace Foundation.Researchers;
@@ -5,7 +6,14 @@ namespace Foundation.Researchers;
 public class Researcher
 {
     public Guid Id { get; set; }
+
+    [Required]
+    [StringLength(30, MinimumLength = 2)]
     public string FirstName { get; set; }
+
+    [Required]
+    [StringLength(30, MinimumLength = 2)]
     public string LastName { get; set; }
+
     public List<Anomaly> Studies { get; set; }
 }
